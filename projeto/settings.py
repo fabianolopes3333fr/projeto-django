62d3505823bcd0f917c3aa276437334be4549a1b
fabiano lopes <meuiptvbr@gmail.com>
2022-07-29 21:17:35 +0200

    alt

--- projeto/settings.py
+++ projeto/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_custon',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Fabiano lopes',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Fabiano lopes',
+    })
